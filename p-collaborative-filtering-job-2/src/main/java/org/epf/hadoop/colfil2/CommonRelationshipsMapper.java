4d9903e5d81a427b556efd66fd7b13f61e3d4ab1
jmaxence <deathloadtv@gmail.com>
2025-01-17 13:40:07 +0100

    modification job2 + ajout des .jar

--- p-collaborative-filtering-job-2/src/main/java/org/epf/hadoop/colfil2/CommonRelationshipsMapper.java
+++ p-collaborative-filtering-job-2/src/main/java/org/epf/hadoop/colfil2/CommonRelationshipsMapper.java
@@ -6,30 +6,39 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CommonRelationshipsMapper extends Mapper<LongWritable, Text, UserPair, IntWritable> {
 
+    @Override
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String line = value.toString();
         String[] tokens = line.split("\t");
+
+        if (tokens.length < 2) {
+            return;
+        }
+
         String firstUser = tokens[0];
         String[] secondUserAndRelationship = tokens[1].split(",");
-        List<String> relationships = Arrays.asList(secondUserAndRelationship);
 
-        for (int i = 0; i < relationships.size(); i++) {
-            for (int j = i + 1; j < relationships.size(); j++) {
-                UserPair userPair = new UserPair(relationships.get(i), relationships.get(j));
+        Set<String> relationshipsSet = new HashSet<>();
+        for (String relationship : secondUserAndRelationship) {
+            relationshipsSet.add(relationship.trim());
+        }
+
+        String[] relationships = relationshipsSet.toArray(new String[0]);
+        for (int i = 0; i < relationships.length; i++) {
+            for (int j = i + 1; j < relationships.length; j++) {
+                UserPair userPair = new UserPair(relationships[i], relationships[j]);
                 context.write(userPair, new IntWritable(1));
             }
         }
 
-        for (String relationship : relationships) {
+        for (String relationship : relationshipsSet) {
             UserPair userPair = new UserPair(firstUser, relationship);
             context.write(userPair, new IntWritable(0));
         }
     }
-
 }
-
--- p-collaborative-filtering-job-2/src/main/java/org/epf/hadoop/colfil2/CommonRelationshipsReducer.java
+++ p-collaborative-filtering-job-2/src/main/java/org/epf/hadoop/colfil2/CommonRelationshipsReducer.java
@@ -1,26 +1,26 @@
 package org.epf.hadoop.colfil2;
 
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-
 import java.io.IOException;
 
 public class CommonRelationshipsReducer extends Reducer<UserPair, IntWritable, UserPair, IntWritable> {
+
+    @Override
     protected void reduce(UserPair key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-        int comp = 0;
+        int count = 0;
+        boolean isValid = true;
+
         for (IntWritable value : values) {
-            if (value.get() == 1) {
-                comp++;
-            }else {
-                comp = 0;
+            if (value.get() == 0) {
+                isValid = false;
                 break;
             }
+            count += value.get();
         }
 
-        if (comp > 0) {
-            context.write(key, new IntWritable(comp));
+        if (isValid && count > 0) {
+            context.write(key, new IntWritable(count));
         }
     }
-
 }
